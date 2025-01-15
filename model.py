2f0d3bb1ace32ac98749c048a0016b4dc9e18c04
LeBlinn <joshyblinn@gmail.com>
2025-01-14 20:47:20 -0500

    done

--- model.py
+++ model.py
@@ -14,4 +14,4 @@ def predict():
 
 if __name__ == "__main__":
     print("Server starting...")
-    waitress.serve(app, host='127.0.0.1', port=5000)
+    waitress.serve(app, host='0.0.0.0', port=5000)
